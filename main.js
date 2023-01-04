67bef0f69caf5c526cad82917c28308076b72ea6
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-04 14:24:56 -0800

    checkpoint

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import * as THREE from 'three';
 let speed = 0;
 let position = 0;
 let rounded = 0;
+let diff = 0;
 let block = document.getElementById('block');
 
 window.addEventListener('wheel', (e) => {
@@ -14,13 +15,9 @@ window.addEventListener('wheel', (e) => {
 function animate() {
   position += speed;
   speed *= 0.8; // Create inertia 
-
   rounded = Math.round(position);
-  
-  let diff = rounded - position;
-
+  diff = rounded - position; // Generate sticky scroll
   position += diff * 0.025;
-
   block.style.transform = `translate(0, ${position*100}px)`;
   window.requestAnimationFrame(animate);
 }
