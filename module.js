6ed83f957da24b0fe5e3396dd74173ab6d977461
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-06 10:48:01 -0800

    refactor

--- module.js
+++ module.js
@@ -12,25 +12,20 @@ import {
 
 export default class Sketch {
     constructor(options) {
+        // Props
         this.width = window.innerWidth;
         this.height = window.innerHeight;
+        this.paused = false;
         this.time = 0;
         this.speed = 0;
-        this.targetSpeed = 0;
-        this.mouse = new THREE.Vector2();
-        this.followMouse = new THREE.Vector2();
-        this.prevMouse = new THREE.Vector2();
-
-        this.paused = false;
-
         this.materials = [];
         this.meshes = [];
         this.groups = [];
-
-        // https://threejs.org/docs/?q=Scene#api/en/scenes/Scene
+        
+        // Scene - https://threejs.org/docs/?q=Scene#api/en/scenes/Scene
         this.scene = new THREE.Scene();
 
-        // https://threejs.org/docs/#api/en/renderers/WebGLRenderer
+        // Renderer - https://threejs.org/docs/#api/en/renderers/WebGLRenderer
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
             alpha: true
@@ -43,7 +38,7 @@ export default class Sketch {
         this.container = options.dom;
         this.container.appendChild(this.renderer.domElement);
 
-        // https://threejs.org/docs/?q=PerspectiveCamera#api/en/cameras/PerspectiveCamera
+        // Camera - https://threejs.org/docs/?q=PerspectiveCamera#api/en/cameras/PerspectiveCamera
         this.camera = new THREE.PerspectiveCamera(
             70,
             this.width / this.width,
@@ -53,51 +48,16 @@ export default class Sketch {
         this.camera.position.set(0, 0, 2);
         this.camera.lookAt(0, 0, 0);
 
-        // https://threejs.org/docs/?q=OrbitControls#examples/en/controls/OrbitControls
+        // Controls -  https://threejs.org/docs/?q=OrbitControls#examples/en/controls/OrbitControls
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         
+        // Init
         this.settings();
-        this.setupResize();
-        this.mouseMove();
-
-        this.addObjects();
+        this.createObjects();
+        this.handleResize();
+        this.handleImages();
         this.resize();
         this.animate();
-        this.handleImages();
-    }
-
-    handleImages() {
-        this.images = [...document.querySelectorAll('img')];
-        this.images.forEach((img, index) => {
-            let mat = this.material.clone();
-            this.materials.push(mat);
-            let group = new THREE.Group();
-            mat.uniforms.texture1.value = new THREE.TextureLoader().load(img.attributes[0].value);
-            mat.uniforms.texture1.value.needsUpdate = true;
-
-            console.log(mat.uniforms);
-
-            // 1.5 aspect ratio of image
-            let geo = new THREE.PlaneGeometry(1.5, 1, 20, 20);
-            let mesh = new THREE.Mesh(geo, mat);
-            group.add(mesh);
-            this.groups.push(group);
-            this.scene.add(group);
-            this.meshes.push(mesh);
-            mesh.position.y = index * 1.2;
-
-            group.rotation.x = -.3;
-            group.rotation.y = -.3;
-            group.rotation.z = -.1;
-        });
-    }
-
-    mouseMove() {
-        let that = this;
-        this.container.addEventListener('mousemove', (e) => {
-            this.mouse.x = (e.clientX / this.width);
-            this.mouse.y = 1. - (e.clientY / this.Height);
-        });
     }
 
     settings() {
@@ -109,35 +69,7 @@ export default class Sketch {
         this.gui.add(this.settings, "progress", 0, 1, 0.01);
     }
 
-    setupResize() {
-        window.addEventListener('resize', this.resize.bind(this));
-    }
-
-    resize() {
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
-        this.renderer.setSize(this.width, this.height);
-        this.camera.aspect = this.width / this.height;
-
-        this.imageAspect = 853 / 1280;
-        let a1, a2;
-        if (this.camera.aspect > this.imageAspect) {
-            a1 = (this.width / this.height) * this.imageAspect;
-            a2 = 1;
-        } else {
-            a1 = 1;
-            a2 = (this.height / this.width) / this.imageAspect;
-        }
-
-        this.material.uniforms.resolution.value.x = this.width;
-        this.material.uniforms.resolution.value.y = this.height;
-        this.material.uniforms.resolution.value.z = a1;
-        this.material.uniforms.resolution.value.w = a2;
-
-        this.camera.updateProjectionMatrix();
-    }
-
-    addObjects() {
+    createObjects() {
         let that = this;
         this.material = new THREE.ShaderMaterial({
             extensions: {
@@ -172,17 +104,70 @@ export default class Sketch {
         });
     }
 
+    handleResize() {
+        window.addEventListener('resize', this.resize.bind(this));
+    }
+
+    handleImages() {
+        this.images = [...document.querySelectorAll('img')];
+        this.images.forEach((img, index) => {
+            let mat = this.material.clone();
+            this.materials.push(mat);
+            let group = new THREE.Group();
+            mat.uniforms.texture1.value = new THREE.TextureLoader().load(img.attributes[0].value);
+            mat.uniforms.texture1.value.needsUpdate = true;
+
+            // 1.5 aspect ratio of image
+            let geo = new THREE.PlaneGeometry(1.5, 1, 20, 20);
+            let mesh = new THREE.Mesh(geo, mat);
+            group.add(mesh);
+            this.groups.push(group);
+            this.scene.add(group);
+            this.meshes.push(mesh);
+            mesh.position.y = index * 1.2;
+
+            group.rotation.x = -.3;
+            group.rotation.y = -.3;
+            group.rotation.z = -.1;
+        });
+    }
+
+    resize() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.renderer.setSize(this.width, this.height);
+        this.camera.aspect = this.width / this.height;
+
+        this.imageAspect = 853 / 1280;
+        let a1, a2;
+        if (this.camera.aspect > this.imageAspect) {
+            a1 = (this.width / this.height) * this.imageAspect;
+            a2 = 1;
+        } else {
+            a1 = 1;
+            a2 = (this.height / this.width) / this.imageAspect;
+        }
+
+        this.material.uniforms.resolution.value.x = this.width;
+        this.material.uniforms.resolution.value.y = this.height;
+        this.material.uniforms.resolution.value.z = a1;
+        this.material.uniforms.resolution.value.w = a2;
+
+        this.camera.updateProjectionMatrix();
+    }
+
     stop() {
         this.paused = true;
     }
 
     play() {
         this.paused = false;
-        this.render();
+        this.animate();
     }
 
     animate() {
         this.time += 0.05;
+        this.paused = false;
 
         if (this.materials) {
             this.materials.forEach(m => {
