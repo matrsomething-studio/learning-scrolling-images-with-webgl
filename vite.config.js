c75c69c80f84a37beb5f87abfd0873dd01cf2ff3
Mat Ruszczycky <mat_ruszczycky@apple.com>
2023-01-07 13:50:46 -0800

    refactor

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [glsl()]
+  plugins: [glsl()],
+  publicDir: "assets"
 });
